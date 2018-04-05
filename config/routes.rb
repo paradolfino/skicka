b58d08007636b0774fba9fe5e8d7992d828480b9
Viktharien <viktharien@zoho.com>
2018-04-05 21:31:57 +0000

    affix index action as root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root => 'messages#index'
+  root 'messages#index'
   get '/messages/:id' => 'messages#show', as: :message
   post '/messages' => 'messages#create'
 end
